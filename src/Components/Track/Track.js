83de2cd10d2bad7b37ee79888bbccca179f8167f
jhammad <javier.hammad@gmail.com>
2023-01-31 20:17:45 +0000

    Components created

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+    render() {
+        return (
+        <div className="Track">
+        <div className="Track-information">
+          {/* <h3><!-- track name will go here --></h3> */}
+          {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+        </div>
+        {/* <button class="Track-action"><!-- + or - will go here --></button> */}
+      </div>
+        );
+    }
+    renderAction() {
+        if (this.props.isRemoval) {
+          return <a className="Track-action">-</a>
+        } else {
+          return <a className="Track-action">+</a>
+        }
+      }
+    }
+export default Track;
